614b45d96517b5fc89f7d88e692e1c923b8ca8eb
jane arc <jane.arc@pobox.com>
2024-09-04 10:18:39 -0700

    +1 test

--- transport/backend_test.go
+++ transport/backend_test.go
@@ -9,6 +9,32 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// this is for NewTransport, which is different than NewService, which is a wrapper
+// around the transport backend(s)
+func TestNewTransport(t *testing.T) {
+	//	func NewTransport(configFileName, versionFileName, secretsFileName string) (*Transport, error) {
+	configFileName := "config/config.yml"
+	versionFileName := "config/version.yml"
+	secretsFileName := "config/secrets.yml"
+
+	root := os.Getenv("ATC_ROOT")
+
+	// concatenate the working directory with our relative filename
+	configFileName = filepath.Join(root, configFileName)
+	versionFileName = filepath.Join(root, versionFileName)
+	secretsFileName = filepath.Join(root, secretsFileName)
+
+	// we're testing the transport here so we're not going to go through all the config tests
+	c, err := transport.LoadConfig(configFileName, versionFileName)
+
+	assert.Nil(t, err)
+
+	// NewTransport takes a *config and a string for the secrets file
+	_, err = transport.NewTransport(c, secretsFileName)
+
+	assert.Nil(t, err)
+}
+
 func TestLoadSecrets(t *testing.T) {
 	//	func LoadSecrets(secretsFileName string) (*Secrets, error) {
 	secretsFileName := "config/secrets.yml"
