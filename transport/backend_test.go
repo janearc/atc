e4f883480a2ddfc838e742fb6b7dbb849972c6cb
jane arc <jane.arc@pobox.com>
2024-09-04 10:23:31 -0700

    +1 test

--- transport/backend_test.go
+++ transport/backend_test.go
@@ -28,11 +28,13 @@ func TestNewTransport(t *testing.T) {
 	c, err := transport.LoadConfig(configFileName, versionFileName)
 
 	assert.Nil(t, err)
+	assert.NotNil(t, c)
 
 	// NewTransport takes a *config and a string for the secrets file
-	_, err = transport.NewTransport(c, secretsFileName)
+	backend, err := transport.NewTransport(c, secretsFileName)
 
 	assert.Nil(t, err)
+	assert.NotNil(t, backend)
 }
 
 func TestLoadSecrets(t *testing.T) {
@@ -44,4 +46,34 @@ func TestLoadSecrets(t *testing.T) {
 	_, err := transport.LoadSecrets(secretsFileName)
 
 	assert.Nil(t, err)
+}
+
+func TestTransport_GetAuthURL(t *testing.T) {
+	//	func (t *Transport) GetAuthURL() string {
+	configFileName := "config/config.yml"
+	versionFileName := "config/version.yml"
+	secretsFileName := "config/secrets.yml"
+
+	root := os.Getenv("ATC_ROOT")
+
+	// concatenate the working directory with our relative filename
+	configFileName = filepath.Join(root, configFileName)
+	versionFileName = filepath.Join(root, versionFileName)
+	secretsFileName = filepath.Join(root, secretsFileName)
+
+	c, err := transport.LoadConfig(configFileName, versionFileName)
+
+	assert.Nil(t, err)
+
+	// NewTransport takes a *config and a string for the secrets file
+	backend, err := transport.NewTransport(c, secretsFileName)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, backend)
+
+	// this is a test so we're not going to worry about the error
+	url := backend.GetAuthURL()
+
+	// not super clear on how to determine whether the url is valid but i think this works for now
+	assert.NotNil(t, url)
 }
